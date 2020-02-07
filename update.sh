1e2a262b83ec9ce16eeecdd7af06c85936728f29
Thomas Roovers <troovers@e-sites.nl>
2020-02-07 15:17:58 +0100

    Update script

--- update.sh
+++ update.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 git fetch --tags
+git tag
 
-read -p -r 'Which tag would you like to switch to? ' tag
+read -r -p 'Which tag would you like to switch to? ' tag
 
+git reset --hard origin/master
 git checkout tags/$tag
 npm install
 npm run build
